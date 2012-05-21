b3c129230ef2160d170b4ee6316039c826d90dee
Lech Wilczynski <lech.wilczynski@gmail.com>
2012-05-21 17:46:08 +0200

    add build script and requirejs optimizer mock bootdata module

---
+++ build/app.build.js
@@ -0,0 +1,16 @@
+// https://github.com/jrburke/r.js/blob/master/build/example.build.js
+({
+    appDir: "../",
+    baseUrl: "js",
+    dir: "../../amdbuild",
+    modules: [
+        {
+            name: "main"
+        }
+    ],
+    paths : {
+        backbone    : "libs/backbone",
+        jquery      : "libs/jquery",
+        underscore  : "libs/underscore"
+    }
+})
---
+++ js/bootdata.js
@@ -0,0 +1,10 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: Lex
+ * Date: 21.05.12
+ * Time: 17:33
+ * To change this template use File | Settings | File Templates.
+ */
+define(function () {
+    return {};
+});
